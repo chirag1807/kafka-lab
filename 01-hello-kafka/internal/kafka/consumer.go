ea291c284c3441461a76920ddf2110af7d70e907
Chirag <chiragmakwana1807@gmail.com>
2026-06-16 22:07:48 +0530

    implement Kafka consumer continuous polling logic with record commitment

--- 01-hello-kafka/internal/kafka/consumer.go
+++ 01-hello-kafka/internal/kafka/consumer.go
@@ -16,11 +16,22 @@ func NewConsumer() (*kgo.Client, error) {
 }
 
 func Poll(client *kgo.Client) {
-	fetches := client.PollFetches(
-		context.Background(),
-	)
+    for {
+        fetches := client.PollFetches(context.Background())
 
-	fetches.EachRecord(func(r *kgo.Record) {
-		fmt.Println(string(r.Value))
-	})
+        if errs := fetches.Errors(); len(errs) > 0 {
+            for _, err := range errs {
+                fmt.Println("fetch error:", err)
+            }
+            continue
+        }
+
+        fetches.EachRecord(func(r *kgo.Record) {
+            fmt.Println(string(r.Value))
+
+            if err := client.CommitRecords(context.Background(), r); err != nil {
+                fmt.Println("commit error:", err)
+            }
+        })
+    }
 }
